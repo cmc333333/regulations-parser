515937cddb796d7c5b54524cdbe68b5f0c9e0945
CM Lubinski <christopher.lubinski@gsa.gov>
2015-08-17 16:28:57 +0000

    Heuristic to dock solutions with different markers appearing at the same level

--- regparser/tree/depth/heuristics.py
+++ regparser/tree/depth/heuristics.py
@@ -22,7 +22,7 @@ def prefer_multiple_children(solutions, weight=1.0):
     return result
 
 
-def prefer_types_on_same_level(solutions, weight=1.0):
+def prefer_same_types_same_levels(solutions, weight=1.0):
     """Dock solutions which have the same type of marker appearing at multiple
     depths. This _does_ occur, but it's not often."""
     result = []
@@ -35,6 +35,24 @@ def prefer_types_on_same_level(solutions, weight=1.0):
         for depths in type_depths.values():
             total += len(depths)
             flags += len(depths) - 1
+
+        result.append(solution.copy_with_penalty(weight * flags / total))
+    return result
+
+
+def prefer_diff_types_diff_levels(solutions, weight=1.0):
+    """Dock solutions which have different markers appearing at the same
+    level. This also occurs, but not often."""
+    result = []
+    for solution in solutions:
+        depth_types = defaultdict(set)
+        for par in solution.assignment:
+            depth_types[par.depth].add(par.typ)
+
+        flags, total = 0, 0
+        for types in depth_types.values():
+            total += len(types)
+            flags += len(types) - 1
 
         result.append(solution.copy_with_penalty(weight * flags / total))
     return result
